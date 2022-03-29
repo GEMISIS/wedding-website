a47e84c86f99f211fda128bc3dcc9a92692ec38d
Gerald McAlister <gemisis@users.noreply.github.com>
2022-03-28 21:08:49 -0700

    Add Excel Spreadsheet Parser
    [Problem]
    Need to add support for parsing Excel spreadsheets.
    
    [Solution]
    Added node-xlsx and a basic file parse to it.
    
    [Testing]
    Manually tested and confirmed proper reading of files from AislePlanner.

--- packages/guest-list-transformer/src/main.ts
+++ packages/guest-list-transformer/src/main.ts
@@ -1,19 +1,29 @@
-
 import yargs = require('yargs');
+import xlsx from 'node-xlsx';
 import { Options } from 'yargs';
 import { hideBin } from 'yargs/helpers';
+
+const rootDir: string = `${__dirname}/../../..`;
 
 interface ArgDefs {
   [key: string]: Options;
 }
-
 const argDefs: ArgDefs = {
   'filename': {
     type: 'string',
     default: 'guests.xlsx'
   },
 }
-
 const argv = yargs(hideBin(process.argv)).options(argDefs).parseSync();
 
-console.log(`Hello world: ${argv.filename}`);
+// Find the specific sheet within the spreadsheet we want.
+const worksheet = xlsx.parse(`${rootDir}/${argv.filename}`);
+const guestSheet = worksheet.find(sheet => sheet.name.toLowerCase() == 'guest parties');
+
+if (guestSheet) {
+  for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+      console.log(`${guestSheet.data[i + j * 5]}`);
+    }
+  }
+}
